747052210c2462ab7f31723d59766db0c60c1f5b
mal1911 <ma1911@mail.ru>
2019-05-05 19:14:38 +0300

    убрал init

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import App from './components/app/app';
 
-const init = () => {
-  render(
-      <App/>,
-      document.getElementById(`root`)
-  );
-};
-
-init();
+render(<App/>, document.getElementById(`root`));
