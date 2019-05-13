a2ad0b9e66e43411f08fd16666bd84ad1970d163
ma1911-ws025 <ma1911@mail.ru>
2019-05-13 10:09:40 +0300

    Замечания исправлены

--- src/components/card/card.test.js
+++ src/components/card/card.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Card from './card';
 
+const mock = `Beautiful & luxurious apartment at great location`;
+
 it(`Card correctly renders`, () => {
   const tree = renderer
     .create(<Card
-      title={``}
+      title={mock}
     />).toJSON();
   expect(tree).toMatchSnapshot();
 });
